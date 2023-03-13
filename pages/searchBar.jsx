fa7c7ab79f1360e51d67f3be8f17038b010bb2bf
Mckay Anderson <andmckay01@gmail.com>
2023-03-13 10:11:33 -0600

    fixed outline and made the dark theme the default

--- pages/searchBar.jsx
+++ pages/searchBar.jsx
@@ -12,7 +12,7 @@ const Search = ({ handleSubmit }) => {
 
             <div className="w-full relative">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" 
+                    <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="currentColor" 
                     viewBox="0 0 20 20" 
                     xmlns="http://www.w3.org/2000/svg"><path 
                     fillRule="evenodd" 
@@ -22,13 +22,13 @@ const Search = ({ handleSubmit }) => {
                 <input type="text"
                 id="search"
                 name="search"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block 
-                w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                className="bg-gray-700 border border-gray-600 text-gray-100 text-sm rounded-lg block outline 
+                w-full pl-10 p-2.5 placeholder-gray-400 outline-transparent focus:border-blue-700" 
                 placeholder="Ask a question"
                 required></input>
             </div>
 
-            <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800">
                 submit
             </button>
         </form>
